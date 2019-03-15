70edd07de7cc0e7fbee9b750b2cef55427bb9d16
guoste20 <stevewangguo@gmail.com>
2019-03-15 12:44:31 -0700

    epic

--- app/src/main/java/com/example/noahcrieslol/AddEmotion.java
+++ app/src/main/java/com/example/noahcrieslol/AddEmotion.java
@@ -4,9 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.MultiAutoCompleteTextView;
 
 public class AddEmotion extends AppCompatActivity {
+
+    Button submit;
+    EditText Color;
+    EditText Emotion;
+    EditText Date;
+    EditText Time;
+    EditText Reason;
+
     private static final String[] COUNTRIES = new String[] {
             "nice", "pie", "bad", "lie"
     };
@@ -19,9 +29,6 @@ public class AddEmotion extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, COUNTRIES);
         editText.setAdapter(adapter);
 
-        /*String[] countries = getResources().getStringArray(R.array.countries_array);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, countries);
-        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.AutoComplete);
-        textView.setAdapter(adapter);*/
+        submit = (Button) findViewById(R.id.Submit);
     }
 }
---
+++ app/src/main/java/com/example/noahcrieslol/DBHelper.java
@@ -0,0 +1,57 @@
+package com.example.noahcrieslol;
+
+import android.content.Context;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "swag.db";
+    public static final String TABLE_NAME = "Emotion";
+    public static final String col_1 = "emotion";
+    public static final String col_2 = "color";
+    public static final String col_3 = "date";
+    public static final String col_4 = "time";
+    public static final String col_5 = "reason";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, 2);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, emotion TEXT, color TEXT, date TEXT, time TEXT, reason TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean addEmotion(String emotion, String color, String date, String time, String reason) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(col_1, emotion);
+        contentValues.put(col_2, color);
+        contentValues.put(col_3, date);
+        contentValues.put(col_4, time);
+        contentValues.put(col_5, reason);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if (result == -1) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    public Cursor getEmotion() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor data = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+        return data;
+    }
+}
